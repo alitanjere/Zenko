abb21ebf244d00f68226f6bca7d2935574b2debc
Frantag15 <129010393+Frantag15@users.noreply.github.com>
2025-05-12 10:15:35 -0300

    Add files via upload

---
+++ Zenko.sql
@@ -0,0 +1,10 @@
+INSERT INTO Lista_Precio_Telas (TelaCodigo, TelaNombre, Costo) VALUES ('TEL-0001', 'Poplín blanco', 780.50);
+INSERT INTO Lista_Precio_Telas (TelaCodigo, TelaNombre, Costo) VALUES ('TEL-0002', 'Jersey algodón', 1120.00);
+INSERT INTO Lista_Precio_Telas (TelaCodigo, TelaNombre, Costo) VALUES ('TEL-0003', 'Gabardina lisa', 1450.75);
+INSERT INTO Lista_Precio_Telas (TelaCodigo, TelaNombre, Costo) VALUES ('TEL-0004', 'Denim azul', 1580.00);
+INSERT INTO Lista_Precio_Telas (TelaCodigo, TelaNombre, Costo) VALUES ('TEL-0005', 'Frisa liviana', 1290.30);
+INSERT INTO Lista_Precio_Telas (TelaCodigo, TelaNombre, Costo) VALUES ('TEL-0006', 'Rib elastizado', 880.90);
+INSERT INTO Lista_Precio_Telas (TelaCodigo, TelaNombre, Costo) VALUES ('TEL-0007', 'Modal estampado', 1680.00);
+INSERT INTO Lista_Precio_Telas (TelaCodigo, TelaNombre, Costo) VALUES ('TEL-0008', 'Lycra deportiva', 1985.60);
+INSERT INTO Lista_Precio_Telas (TelaCodigo, TelaNombre, Costo) VALUES ('TEL-0009', 'Interlock grueso', 1230.00);
+INSERT INTO Lista_Precio_Telas (TelaCodigo, TelaNombre, Costo) VALUES ('TEL-0010', 'Algodón 100% natural', 950.00);
