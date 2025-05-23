82ac3a4a5bdee04cc6349111e4d2bf6fcf4965f7
Frantag15 <48178331@ort>
2025-05-23 12:14:52 -0300

    se agregaron las tablas de productos y productosinsumo

--- Zenko.sql
+++ Zenko.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE Zenko;
+ DATABASE Zenko;
 GO
 
 -- Usar la base de datos recién creada
@@ -19,5 +19,23 @@ CREATE TABLE Insumos (
     FOREIGN KEY (IdTipoInsumo) REFERENCES Tipos_Insumo(IdTipoInsumo)
 );
 
-INSERT INTO Tipos_Insumo (IdTipoInsumo, Nombre, CodigoPrefijo) VALUES (1, 'Tela', 'V23');
-INSERT INTO Tipos_Insumo (IdTipoInsumo, Nombre, CodigoPrefijo) VALUES (2, 'Avio', 'I18');
+CREATE TABLE Productos (
+    IdProducto INT PRIMARY KEY IDENTITY(1,1),
+    Nombre NVARCHAR(100) NOT NULL,
+    CodigoProducto NVARCHAR(20) NOT NULL UNIQUE
+);
+
+CREATE TABLE ProductoInsumo (
+    IdProducto INT NOT NULL,
+    CodigoInsumo NVARCHAR(20) NOT NULL,
+    Cantidad DECIMAL(10,2) NOT NULL, -- Por ejemplo, metros de tela o unidades de botón
+
+    PRIMARY KEY (IdProducto, CodigoInsumo),
+    FOREIGN KEY (IdProducto) REFERENCES Productos(IdProducto),
+    FOREIGN KEY (CodigoInsumo) REFERENCES Insumos(CodigoInsumo)
+);
+
+
+--Inserts futuros
+--INSERT INTO Tipos_Insumo (IdTipoInsumo, Nombre, CodigoPrefijo) VALUES (1, 'Tela', 'V23');
+--INSERT INTO Tipos_Insumo (IdTipoInsumo, Nombre, CodigoPrefijo) VALUES (2, 'Avio', 'I18');
