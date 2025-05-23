2e3491c266551912ebb093dee09c3ae604665147
Frantag15 <48178331@ort>
2025-05-23 10:40:02 -0300

    la navbar tiene color

--- Models/Insumo.cs
+++ Models/Insumo.cs
@@ -1,7 +1,8 @@
+using System;
 public class Insumo
 {
     public string CodigoInsumo { get; set; }
     public int IdTipoInsumo { get; set; }
     public decimal Costo { get; set; }
-    public datetime FechaRegistro { get; set; }
+    public DateTime FechaRegistro { get; set; }
 }
--- obj/Debug/net8.0/ZenkoApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/ZenkoApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ZenkoApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5f66ba9e7b299725431073d9a1b73d00838a6e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d60d7c4f6dd0b13fd850a1befd5c8e90d5a4dc28")]
 [assembly: System.Reflection.AssemblyProductAttribute("ZenkoApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ZenkoApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
